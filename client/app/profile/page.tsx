7a889fcbc3aaac17466e46532f25d340751d5d53
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-11-20 15:58:07 +0530

    update: profile page: ui fixed except bento image grid

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -186,7 +186,7 @@ const DevMorphixWebsite = () => {
                       width="100%"
                       height="100%"
                       style={{ border: 0 }}
-                      allowFullScreen=""
+                      allowFullScreen
                       loading="lazy"
                     ></iframe>
                   </div>
