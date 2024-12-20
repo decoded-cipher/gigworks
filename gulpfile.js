15589bcd0e6d8099bb92afa72d90ee9fe0aa8ba2
Arjun Krishna <arjun12345krishna@gmail.com>
2024-12-20 15:42:31 +0530

    feat : [gulp] - Better DX

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,53 @@
 
 const { parallel } = require('gulp');
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 
-const startClient = (cb) => {
-    const client = spawn('npm', ['run', 'dev'], { cwd: 'client', stdio: 'inherit', shell: true });
-    client.on('close', cb);
-}
-
-const startServer = (cb) => {
-    const server = spawn('npm', ['run', 'dev'], { cwd: 'server', stdio: 'inherit', shell: true });
-    server.on('close', cb);
+const runCommand = (command, args, options) => {
+    return new Promise((resolve, reject) => {
+        const proc = exec([command, ...args].join(' '), options, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(stdout);
+            }
+        });
+        proc.stdout.pipe(process.stdout);
+        proc.stderr.pipe(process.stderr);
+    });
 };
 
-const startAll = parallel(startClient, startServer);
-exports.start = startAll;
+const startClient = async () => {
+    try {
+        await runCommand('npm', ['run', 'dev'], { cwd: 'client', stdio: 'inherit', shell: true });
+        console.log('Client started successfully');
+    } catch (error) {
+        console.error('Failed to start client:', error);
+    }
+};
+
+const startServer = async () => {
+    try {
+        await runCommand('npm', ['run', 'dev'], { cwd: 'server', stdio: 'inherit', shell: true });
+        console.log('Server started successfully');
+    } catch (error) {
+        console.error('Failed to start server:', error);
+    }
+};
+
+const startAdmin = async () => {
+    try {
+        await runCommand('npm', ['run', 'dev'], { cwd: 'admin', stdio: 'inherit', shell: true });
+        console.log('Admin started successfully');
+    } catch (error) {
+        console.error('Failed to start admin:', error);
+    }
+};
+
+
+
+const startAll = parallel(startClient, startServer, startAdmin);
+exports.startAll = startAll;
+
+exports.startClient = startClient;
+exports.startServer = startServer;
+exports.startAdmin = startAdmin;
