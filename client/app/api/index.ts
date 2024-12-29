f8bedd3a7e70df2dd94f225a2819202881f8c53c
Badhusha <badhusha.shaji22@gmail.com>
2024-12-29 21:08:36 +0530

    api integration - authentication

--- client/app/api/index.ts
+++ client/app/api/index.ts
@@ -23,10 +23,76 @@ export const fetchBusinessesByCategory = async (categoryId: string) => {
     throw error;
   }
 };
+
 export const fetchBusinessesByslug = async (slug: string) => {
   try {
     const response = await axios.get(
       `${process.env.NEXT_PUBLIC_API_URL}/api/v1/business/${slug}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching businesses:', error);
+    throw error;
+  }
+};
+
+export const CreateBusiness = async (data: any) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/business`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching businesses:', error);
+    throw error;
+  }
+};
+
+export const UserRegister = async (data: any) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/register`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching businesses:', error);
+    throw error;
+  }
+};
+
+export const UserLogin = async (data: any) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
+
+export const VerifyLoginOTP = async (data: any) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/verify/login`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error verifying OTP:', error);
+    throw error;
+  }
+};
+
+export const UserLogout = async (data: any) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/logout`,
+      data
     );
     return response.data;
   } catch (error) {
