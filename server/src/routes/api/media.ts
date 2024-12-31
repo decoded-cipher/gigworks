4bc8ec966e4b0cb9dde99242a20911ca335834c3
Arjun Krishna <arjun12345krishna@gmail.com>
2024-12-31 13:23:53 +0530

    feat : upload Profile Asset

--- server/src/routes/api/media.ts
+++ server/src/routes/api/media.ts
@@ -2,8 +2,13 @@
 import { Hono } from 'hono';
 const router = new Hono();
 
+import { verifyToken } from '../../middleware/authentication';
+
 import { nanoid } from 'nanoid';
-import { generatePreSignedUrl } from '../../services/media';
+import { profileMedia } from '../../config/database/schema';
+import { ProfileMedia } from '../../config/database/interfaces';
+
+import { generatePreSignedUrl, saveProfileMedia } from '../../services/media';
 
 
 
@@ -24,7 +29,7 @@ router.get('/get-presigned-url', async (c) => {
         const type = c.req.query('type');
         const category = c.req.query('category');
 
-        const categories = ['avatar', 'banner', 'license', 'identity'];
+        const categories = ['avatar', 'banner', 'media', 'license', 'identity'];
         const allowedFileTypes = ['image', 'video'];
 
         if (!type || !category) {
@@ -61,6 +66,50 @@ router.get('/get-presigned-url', async (c) => {
                 assetPath: path,
                 presignedUrl: url
             }
+        }, 201);
+    } catch (error) {
+        return c.json({
+            message: 'Internal Server Error',
+            error: error.message
+        }, 500);
+    }
+});
+
+
+
+/**
+ * @route   POST /api/v1/media
+ * @desc    Upload media to profile
+ * @access  Public
+ * @params  user, media
+ * @return  message, data
+ * @error   400, { error }
+ * @status  201, 400
+ * 
+ * @example /api/v1/media
+ **/
+
+router.post('/', verifyToken, async (c) => {
+    try {
+        const { profile_id, url, description } = await c.req.json();
+
+        if (!profile_id || !url) {
+            return c.json({
+                message: 'Profile ID and URL are required'
+            }, 400);
+        }
+
+        let result = await saveProfileMedia({ profile_id, url, description });
+
+        if (!result) {
+            return c.json({
+                message: 'Error saving media'
+            }, 400);
+        }
+
+        return c.json({
+            message: 'Media saved successfully',
+            data: result
         }, 201);
     } catch (error) {
         return c.json({
