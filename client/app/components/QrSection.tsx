c9196c638eac0341fa81baad15d90e26b0a0db13
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2025-01-21 12:57:57 +0530

    added

--- client/app/components/QrSection.tsx
+++ client/app/components/QrSection.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import QRCodeStyling from 'qr-code-styling';
 import { Share2 } from 'lucide-react';
+import html2canvas from 'html2canvas';
 
 interface QrSectionProps {
   slug: string;
@@ -10,6 +11,7 @@ interface QrSectionProps {
 
 const QrSection: React.FC<QrSectionProps> = ({ slug}) => {
   const qrCodeRef = useRef<HTMLDivElement>(null);
+  const qrCodeContainerRef = useRef<HTMLDivElement>(null);
   const qrCode = useRef<QRCodeStyling | null>(null);
   const currentUrl = `https://gigwork.co.in/${slug}`;
 
@@ -56,15 +58,33 @@ const QrSection: React.FC<QrSectionProps> = ({ slug}) => {
       }
     };
   }, [currentUrl]);
-  const handleShareClick = () => {
-    if (navigator.share) {
-      navigator.share({
-        title: 'Gigwork',
-        text: 'Check out this business on Gigwork!',
-        url: currentUrl,
-      })
-      .then(() => console.log('Successful share'))
-      .catch((error) => console.log('Error sharing', error));
+
+
+
+  const handleShareClick = async () => {
+    if (navigator.share && qrCodeContainerRef.current) {
+      try {
+        const canvas = await html2canvas(qrCodeContainerRef.current);
+        canvas.toBlob((blob) => {
+          if (blob) {
+            const filesArray = [
+              new File([blob], 'qr-code.png', {
+                type: blob.type,
+              }),
+            ];
+
+            navigator.share({
+              title: 'Gigwork',
+              text: `Check out this business on Gigwork!\n${currentUrl}`,
+              files: filesArray,
+            })
+            .then(() => console.log('Successful share'))
+            .catch((error) => console.log('Error sharing', error));
+          }
+        });
+      } catch (error) {
+        console.error('Error generating image for sharing:', error);
+      }
     } else {
       // Fallback for browsers that do not support the Web Share API
       window.open(currentUrl, '_blank');
@@ -73,7 +93,7 @@ const QrSection: React.FC<QrSectionProps> = ({ slug}) => {
 
   return (
     <div>
-      <div className="w-60 sm:w-80 mx-auto bg-white border rounded-lg shadow-lg p-6">
+      <div ref={qrCodeContainerRef} className="w-60 sm:w-80 mx-auto bg-white border rounded-lg shadow-lg p-6">
         <div className="space-y-4">
           <h2 className="text-center font-medium mb-4">Scan Me</h2>
           <div ref={qrCodeRef} className="w-full h-auto mx-auto flex justify-center" />
