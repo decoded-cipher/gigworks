fa44b4c8a8ceb875147ae3ceb447602044726cf3
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2025-01-23 10:06:34 +0530

    added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -583,126 +583,131 @@ export default function GigWorkLandingPage() {
         id="partner"
         style={{ scrollMarginTop: "80px" }}
       >
-        <div className="w-full flex flex-col items-center justify-center">
-          <div className="w-full max-w-sm p-4 border rounded-lg shadow sm:p-8 bg-green-900 border-gray-700">
-            <h5 className="mb-4 text-xl font-medium text-white">
-              Standard plan
-            </h5>
-            <div className="flex items-baseline text-gray-200 ">
-              <span className="text-1xl font-semibold line-through">₹</span>
-              <span className="text-3xl font-extrabold tracking-tight line-through">
-                2500
-              </span>
-              <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
-                /year
-              </span>
-            </div>
-            <div className="flex items-baseline text-white">
-              <span className="text-3xl font-semibold">₹</span>
-              <span className="text-5xl font-extrabold tracking-tight">
-                1500
-              </span>
-              <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
-                /year
-              </span>
-            </div>
-            <ul role="list" className="space-y-5 my-7">
-              <li className="flex items-center">
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-green-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
-                <span className="text-base font-normal leading-tight text-gray-400 ms-3">
-                  Your Own Personalized Webpage
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold mb-8 ">
+            Our <span className="text-green-600 ">Pricing</span>
+          </h2>
+          <div className="w-full flex flex-col items-center justify-center text-left">
+            <div className="w-full max-w-sm p-4 border rounded-lg shadow sm:p-8 bg-green-900 border-gray-700">
+              <h5 className="mb-4 text-xl font-medium text-white">
+                Standard plan
+              </h5>
+              <div className="flex items-baseline text-gray-200 ">
+                <span className="text-1xl font-semibold line-through">₹</span>
+                <span className="text-3xl font-extrabold tracking-tight line-through">
+                  2500
                 </span>
-              </li>
-              <li className="flex">
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-green-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
-                <span className="text-base font-normal leading-tight text-gray-400 ms-3">
-                  Showcase your business with a professional portfolio designed
-                  to impress your customers.
+                <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                  /year
                 </span>
-              </li>
-              <li className="flex">
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-green-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
-                <span className="text-base font-normal leading-tight text-gray-400 ms-3">
-                  Boost your online visibility and reach the top with zero
-                  hassle.
+              </div>
+              <div className="flex items-baseline text-white">
+                <span className="text-3xl font-semibold">₹</span>
+                <span className="text-5xl font-extrabold tracking-tight">
+                  1500
                 </span>
-              </li>
-              <li className="flex">
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-green-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
-                <span className="text-base font-normal leading-tight text-gray-400 ms-3">
-                  Seamless WhatsApp Chatbot
+                <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                  /year
                 </span>
-              </li>
-              <li className="flex">
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-green-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
-                <span className="text-base font-normal leading-tight text-gray-400 ms-3">
-                  Let your customers find you instantly through our easy-to-use
-                  WhatsApp-based search tool.
-                </span>
-              </li>
-            </ul>
-            <button
-              type="button"
-              onClick={() => {
-                if (!isLoggedIn()) {
-                  setIsLoginPopupOpen(true);
-                } else {
-                  // check if the profile in local storage is empty or not
-                  const profile = localStorage.getItem("profile");
-                  if (profile) {
-                    // fetch slug from profile and redirect to profile page
-                    const { slug } = JSON.parse(profile);
-                    router.push(`/${slug}`);
-                  } else {
-                    // ask login popup
+              </div>
+              <ul role="list" className="space-y-5 my-7">
+                <li className="flex items-center">
+                  <svg
+                    className="flex-shrink-0 w-4 h-4 text-green-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                  </svg>
+                  <span className="text-base font-normal leading-tight text-gray-400 ms-3">
+                    Your Own Personalized Webpage
+                  </span>
+                </li>
+                <li className="flex">
+                  <svg
+                    className="flex-shrink-0 w-4 h-4 text-green-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                  </svg>
+                  <span className="text-base font-normal leading-tight text-gray-400 ms-3">
+                    Showcase your business with a professional portfolio
+                    designed to impress your customers.
+                  </span>
+                </li>
+                <li className="flex">
+                  <svg
+                    className="flex-shrink-0 w-4 h-4 text-green-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                  </svg>
+                  <span className="text-base font-normal leading-tight text-gray-400 ms-3">
+                    Boost your online visibility and reach the top with zero
+                    hassle.
+                  </span>
+                </li>
+                <li className="flex">
+                  <svg
+                    className="flex-shrink-0 w-4 h-4 text-green-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                  </svg>
+                  <span className="text-base font-normal leading-tight text-gray-400 ms-3">
+                    Seamless WhatsApp Chatbot
+                  </span>
+                </li>
+                <li className="flex">
+                  <svg
+                    className="flex-shrink-0 w-4 h-4 text-green-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                  </svg>
+                  <span className="text-base font-normal leading-tight text-gray-400 ms-3">
+                    Let your customers find you instantly through our
+                    easy-to-use WhatsApp-based search tool.
+                  </span>
+                </li>
+              </ul>
+              <button
+                type="button"
+                onClick={() => {
+                  if (!isLoggedIn()) {
                     setIsLoginPopupOpen(true);
+                  } else {
+                    // check if the profile in local storage is empty or not
+                    const profile = localStorage.getItem("profile");
+                    if (profile) {
+                      // fetch slug from profile and redirect to profile page
+                      const { slug } = JSON.parse(profile);
+                      router.push(`/${slug}`);
+                    } else {
+                      // ask login popup
+                      setIsLoginPopupOpen(true);
+                    }
                   }
-                }
-              }}
-              className="text-white focus:ring-4 focus:outline-none bg-green-700 hover:bg-green-600 focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
-            >
-              Start now
-            </button>
+                }}
+                className="text-white focus:ring-4 focus:outline-none bg-green-700 hover:bg-green-600 focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+              >
+                Start now
+              </button>
+            </div>
           </div>
         </div>
       </section>
