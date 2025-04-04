c78a8dd3ba3a941d018876dd9ce3307e1026bfe7
Nithin Daniel <nithindaniel2018@gmail.com>
2025-04-04 23:33:15 +0530

    feat: add status to Category,SubCategory,SubCategoryOption

--- server/src/routes/api/category.ts
+++ server/src/routes/api/category.ts
@@ -1,17 +1,14 @@
-
-import { Hono } from 'hono';
+import { Hono } from "hono";
 const router = new Hono();
 
-import { seedCategory } from '../../seeder/category';
+import { seedCategory } from "../../seeder/category";
 import {
-    createCategory,
-    getCategories,
-    getCategoryById,
-    updateCategory,
-    enableDisableCategory
-} from '../../services/category';
-
-
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+  enableDisableCategory,
+} from "../../services/category";
 
 /**
  * @route   POST /api/v1/category
@@ -25,31 +22,37 @@ import {
  * @example /api/v1/category
  **/
 
-router.post('/', async (c) => {
-    const { name } = await c.req.json();
+router.post("/", async (c) => {
+  const { name } = await c.req.json();
 
-    if (!name) {
-        return c.json({
-            message: 'Name is required'
-        }, 400);
-    }
+  if (!name) {
+    return c.json(
+      {
+        message: "Name is required",
+      },
+      400
+    );
+  }
 
-    try {
-        const category = await createCategory(name);
-        return c.json({
-            message: 'Category created successfully',
-            // data: category
-        }, 201);
-
-    } catch (error) {
-        return c.json({
-            message: 'Internal Server Error',
-            error: error.message
-        }, 500);
-    }
+  try {
+    const category = await createCategory(name);
+    return c.json(
+      {
+        message: "Category created successfully",
+        // data: category
+      },
+      201
+    );
+  } catch (error) {
+    return c.json(
+      {
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      500
+    );
+  }
 });
-
-
 
 /**
  * @route   GET /api/v1/category
@@ -63,51 +66,69 @@ router.post('/', async (c) => {
  * @example /api/v1/category?page=1&limit=10
  **/
 
-router.get('/', async (c) => {
+router.get("/", async (c) => {
+  const page = Number(c.req.query("page")) || 1;
+  const limit = Number(c.req.query("limit"));
+  const search = c.req.query("search") || "";
+  const hasBusiness = Boolean(c.req.query("hasBusiness")) || false;
+  const status = c.req.query("status");
 
-    const page = Number(c.req.query('page')) || 1;
-    const limit = Number(c.req.query('limit'));
-    const search = c.req.query('search') || '';
-    const hasBusiness = Boolean(c.req.query('hasBusiness')) || false;
+  try {
+    const result = await getCategories(
+      page,
+      limit,
+      search,
+      hasBusiness,
+      status
+    );
 
-    try {
-
-        const result = await getCategories(page, limit, search, hasBusiness);
-
-        if (result.data.length > 0) {
-            return c.json({
-                message: 'Categories fetched successfully',
-                data: {
-                    categories: result.data,
-                    meta: {
-                        params: {
-                            page: page,
-                            limit: limit,
-                            search: search,
-                            hasBusiness: hasBusiness
-                        },
-                        total_count: result.count,
-                        total_pages: Math.ceil(result.count / limit),
-                        previous: page > 1 ? `/api/v1/category?page=${page - 1}&limit=${limit}` : null,
-                        next: result.data.length === limit ? `/api/v1/category?page=${page + 1}&limit=${limit}` : null
-                    }
-                }
-            }, 200);
-        } else {
-            return c.json({
-                message: 'Categories not found'
-            }, 404);
-        }
-
-    } catch (error) {
-        return c.json({
-            message: 'Internal Server Error',
-            error: error.message
-        }, 500);
+    if (result.data.length > 0) {
+      return c.json(
+        {
+          message: "Categories fetched successfully",
+          data: {
+            categories: result.data,
+            meta: {
+              params: {
+                page: page,
+                limit: limit,
+                search: search,
+                hasBusiness: hasBusiness,
+                status: status,
+              },
+              total_count: result.count,
+              total_pages: Math.ceil(result.count / limit),
+              previous:
+                page > 1
+                  ? `/api/v1/category?page=${page - 1}&limit=${limit}`
+                  : null,
+              next:
+                result.data.length === limit
+                  ? `/api/v1/category?page=${page + 1}&limit=${limit}`
+                  : null,
+            },
+          },
+        },
+        200
+      );
+    } else {
+      return c.json(
+        {
+          message: "Categories not found",
+        },
+        404
+      );
     }
+  } catch (error) {
+    return c.json(
+      {
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      500
+    );
+  }
 });
-
-
 
 /**
  * @route   GET /api/v1/category/:id
@@ -117,36 +138,42 @@ router.get('/', async (c) => {
  * @return  message, data
  * @error   400, { error }
  * @status  200, 400
- * 
+ *
  * @example /api/v1/category/1
  **/
 
-router.get('/:id', async (c) => {
-    const { id } = c.req.param();
+router.get("/:id", async (c) => {
+  const { id } = c.req.param();
 
-    try {
-        const category = await getCategoryById(id);
+  try {
+    const category = await getCategoryById(id);
 
-        if (category) {
-            return c.json({
-                message: 'Category fetched successfully',
-                data: category
-            }, 200);
-        } else {
-            return c.json({
-                message: 'Category not found'
-            }, 404);
-        }
-
-    } catch (error) {
-        return c.json({
-            message: 'Internal Server Error',
-            error: error.message
-        }, 500);
+    if (category) {
+      return c.json(
+        {
+          message: "Category fetched successfully",
+          data: category,
+        },
+        200
+      );
+    } else {
+      return c.json(
+        {
+          message: "Category not found",
+        },
+        404
+      );
     }
+  } catch (error) {
+    return c.json(
+      {
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      500
+    );
+  }
 });
-
-
 
 /**
  * @route   PATCH /api/v1/category/:id
@@ -156,43 +183,52 @@ router.get('/:id', async (c) => {
  * @return  message, data
  * @error   400, { error }
  * @status  200, 400
- * 
+ *
  * @example /api/v1/category/1
  **/
 
-router.patch('/:id', async (c) => {
-    const { name } = await c.req.json();
-    const id = c.req.param('id');
+router.patch("/:id", async (c) => {
+  const { name } = await c.req.json();
+  const id = c.req.param("id");
 
-    if (!name) {
-        return c.json({
-            message: 'All fields are required'
-        }, 400);
+  if (!name) {
+    return c.json(
+      {
+        message: "All fields are required",
+      },
+      400
+    );
+  }
+
+  try {
+    const category = await updateCategory(id, name);
+
+    if (category) {
+      return c.json(
+        {
+          message: "Category updated successfully",
+          data: category,
+        },
+        200
+      );
+    } else {
+      return c.json(
+        {
+          message: "Category not found",
+        },
+        404
+      );
     }
-
-    try {
-        const category = await updateCategory(id, name);
-
-        if (category) {
-            return c.json({
-                message: 'Category updated successfully',
-                data: category
-            }, 200);
-        } else {
-            return c.json({
-                message: 'Category not found'
-            }, 404);
-        }
-
-    } catch (error) {
-        return c.json({
-            message: 'Internal Server Error',
-            error: error.message
-        }, 500);
-    }
+  } catch (error) {
+    return c.json(
+      {
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      500
+    );
+  }
 });
-
-
 
 /**
  * @route   PATCH /api/v1/category/:id/:mode
@@ -202,48 +238,57 @@ router.patch('/:id', async (c) => {
  * @return  message, data
  * @error   400, { error }
  * @status  200, 400
- * 
+ *
  * @example /api/v1/category/1
  **/
 
-router.patch('/:id/:mode', async (c) => {
-    const { id, mode } = c.req.param();
+router.patch("/:id/:mode", async (c) => {
+  const { id, mode } = c.req.param();
 
-    if (!id || !mode) {
-        return c.json({
-            message: 'Category ID and MODE are required'
-        }, 400);
-    }
+  if (!id || !mode) {
+    return c.json(
+      {
+        message: "Category ID and MODE are required",
+      },
+      400
+    );
+  }
 
-    let status = 0;
-    switch (mode) {
-        case 'enable':
-            status = 1;
-            break;
-        case 'disable':
-            status = 0;
-            break;
-        default:
-            return c.json({
-                message: 'Invalid mode'
-            }, 400);
-    }
+  let status = 0;
+  switch (mode) {
+    case "enable":
+      status = 1;
+      break;
+    case "disable":
+      status = 0;
+      break;
+    default:
+      return c.json(
+        {
+          message: "Invalid mode",
+        },
+        400
+      );
+  }
 
-    try {
-        await enableDisableCategory(id, status);
-        return c.json({
-            message: 'Category and Sub-categories updated successfully',
-        }, 200);
-
-    } catch (error) {
-        return c.json({
-            message: 'Internal Server Error',
-            error: error.message
-        }, 500);
-    }
+  try {
+    await enableDisableCategory(id, status);
+    return c.json(
+      {
+        message: "Category and Sub-categories updated successfully",
+      },
+      200
+    );
+  } catch (error) {
+    return c.json(
+      {
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      500
+    );
+  }
 });
-
-
 
 /**
  * @route   GET /api/v1/category/seed
@@ -253,7 +298,7 @@ router.patch('/:id/:mode', async (c) => {
  * @return  message, data
  * @error   400, { error }
  * @status  200, 400
- * 
+ *
  * @example /api/v1/category/seed
  **/
 
@@ -271,6 +316,5 @@ router.patch('/:id/:mode', async (c) => {
 //         }, 500);
 //     }
 // });
-
 
 export default router;
