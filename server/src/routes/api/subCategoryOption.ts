1f3d061631a69e76753561668f04a8a8c17fc6f4
Nithin Daniel <nithindaniel2018@gmail.com>
2025-04-05 11:04:45 +0530

    feat: add SubCategoryOption status  PATCH

--- server/src/routes/api/subCategoryOption.ts
+++ server/src/routes/api/subCategoryOption.ts
@@ -1,10 +1,10 @@
-
-import { Hono } from 'hono';
+import { Hono } from "hono";
 const router = new Hono();
 
-import { createSubCategoryOption } from '../../services/subCategoryOption';
-
-
+import {
+  createSubCategoryOption,
+  updateSubCategoryOptionStatus,
+} from "../../services/subCategoryOption";
 
 /**
  * @route   POST /api/v1/sub_category_option
@@ -18,30 +18,74 @@ import { createSubCategoryOption } from '../../services/subCategoryOption';
  * @example /api/v1/sub_category_option
  **/
 
-router.post('/', async (c) => {
-    const { name, sub_category_id } = await c.req.json();
+router.post("/", async (c) => {
+  const { name, sub_category_id } = await c.req.json();
 
-    if (!name || !sub_category_id) {
-        return c.json({
-            message: 'Name and Sub-Category ID are required'
-        }, 400);
-    }
+  if (!name || !sub_category_id) {
+    return c.json(
+      {
+        message: "Name and Sub-Category ID are required",
+      },
+      400
+    );
+  }
 
-    try {
-        const sub_category_option = await createSubCategoryOption(name, sub_category_id);
-        return c.json({
-            message: 'Sub category option created successfully',
-            data: sub_category_option
-        }, 201);
-
-    } catch (error) {
-        return c.json({
-            message: 'Internal Server Error',
-            error: error.message
-        }, 500);
-    }
+  try {
+    const sub_category_option = await createSubCategoryOption(
+      name,
+      sub_category_id
+    );
+    return c.json(
+      {
+        message: "Sub category option created successfully",
+        data: sub_category_option,
+      },
+      201
+    );
+  } catch (error) {
+    return c.json(
+      {
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      500
+    );
+  }
 });
 
+router.patch("/update-status", async (c) => {
+  const { sub_category_option_id, status } = await c.req.json();
 
+  if (!sub_category_option_id || status === undefined) {
+    return c.json(
+      {
+        message: "sub_category_option_id and status are required",
+      },
+      400
+    );
+  }
+
+  try {
+    const updatedOption = await updateSubCategoryOptionStatus(
+      sub_category_option_id,
+      status
+    );
+    return c.json(
+      {
+        message: "Sub category option status updated successfully",
+        data: updatedOption,
+      },
+      200
+    );
+  } catch (error) {
+    return c.json(
+      {
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      500
+    );
+  }
+});
 
 module.exports = router;
